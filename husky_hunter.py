a7a888bc6c58d810f53c0a9ed1111f2378ff505b
Kushal Krishnappa <kushalkrishnappa333@gmail.com>
2025-08-25 16:16:53 -0400

    bug: updated set session

--- husky_hunter.py
+++ husky_hunter.py
@@ -29,9 +29,9 @@ class HuskyHunter:
         self.session = self.set_session()
         self.create_output_directory()
 
-    def set_session(self):
-        self.session = requests.Session()
-        self.session.headers.update({
+    def set_session(self) -> requests.Session:
+        session = requests.Session()
+        session.headers.update({
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
             'Accept-Language': 'en-US,en;q=0.5',
@@ -39,6 +39,7 @@ class HuskyHunter:
             'Connection': 'keep-alive',
             'Upgrade-Insecure-Requests': '1',
         })
+        return session
 
     def create_output_directory(self):
         os.makedirs(self.output_dir, exist_ok=True)
